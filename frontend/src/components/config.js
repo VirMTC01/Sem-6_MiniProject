b57341e8db5a0dd113f50c7aaa126a2918c2eb36
VirMTC01 <121671023+VirMTC01@users.noreply.github.com>
2024-04-29 14:35:24 +0530

    Update config.js

--- frontend/src/components/config.js
+++ frontend/src/components/config.js
@@ -1,3 +1,3 @@
-// const BACKEND_URL = "https://sem-6-miniproject-backend.onrender.com"; 
-const BACKEND_URL = "http://localhost:8000"; 
+const BACKEND_URL = "https://sem-6-miniproject-backend.onrender.com"; 
+// const BACKEND_URL = "http://localhost:8000"; 
 export default BACKEND_URL;
